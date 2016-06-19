fbea0e967146ee4657542fb503658b0af6b919ec
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2016-06-19 11:28:51 +0300

    Indent project.clj

--- project.clj
+++ project.clj
@@ -49,94 +49,94 @@
             [lein-immutant "2.1.0"]
             [lein-sassc "0.10.4"]
             [lein-auto "0.1.2"]]
-   :sassc
-   [{:src "resources/scss/screen.scss"
-     :output-to "resources/public/css/screen.css"
-     :style "nested"
-     :import-path "resources/scss"}]
+  :sassc
+  [{:src         "resources/scss/screen.scss"
+    :output-to   "resources/public/css/screen.css"
+    :style       "nested"
+    :import-path "resources/scss"}]
 
-   :auto
-   {"sassc" {:file-pattern #"\.(scss|sass)$" :paths ["resources/scss"]}}
+  :auto
+  {"sassc" {:file-pattern #"\.(scss|sass)$" :paths ["resources/scss"]}}
 
   :hooks [leiningen.sassc]
   :clean-targets ^{:protect false}
-  [:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
+[:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
 
   :cljsbuild
   {:builds
    {:app
     {:source-paths ["src/cljc" "src/cljs" "env/dev/cljs"]
-     :figwheel true
+     :figwheel     true
      :compiler
-     {:main "predictor.app"
-      :asset-path "/js/out"
-      :output-to "target/cljsbuild/public/js/app.js"
-      :output-dir "target/cljsbuild/public/js/out"
-      :optimizations :none
-      :recompile-dependents false
-      :source-map true
-      :pretty-print true}}
+                   {:main                 "predictor.app"
+                    :asset-path           "/js/out"
+                    :output-to            "target/cljsbuild/public/js/app.js"
+                    :output-dir           "target/cljsbuild/public/js/out"
+                    :optimizations        :none
+                    :recompile-dependents false
+                    :source-map           true
+                    :pretty-print         true}}
     :test
     {:source-paths ["src/cljc" "src/cljs" "test/cljs"]
      :compiler
-     {:output-to "target/test.js"
-      :main "predictor.doo-runner"
-      :optimizations :whitespace
-      :pretty-print true}}
+                   {:output-to     "target/test.js"
+                    :main          "predictor.doo-runner"
+                    :optimizations :whitespace
+                    :pretty-print  true}}
     :min
     {:source-paths ["src/cljc" "src/cljs" "env/prod/cljs"]
      :compiler
-     {:output-to "target/cljsbuild/public/js/app.js"
-      :output-dir "target/uberjar"
-      :externs ["react/externs/react.js"]
-      :optimizations :advanced
-      :pretty-print false
-      :closure-warnings
-      {:externs-validation :off :non-standard-jsdoc :off}}}}}
+                   {:output-to     "target/cljsbuild/public/js/app.js"
+                    :output-dir    "target/uberjar"
+                    :externs       ["react/externs/react.js"]
+                    :optimizations :advanced
+                    :pretty-print  false
+                    :closure-warnings
+                                   {:externs-validation :off :non-standard-jsdoc :off}}}}}
 
   :figwheel
   {:http-server-root "public"
-   :nrepl-port 7002
+   :nrepl-port       7002
    :reload-clj-files false
-   :css-dirs ["resources/public/css"]
+   :css-dirs         ["resources/public/css"]
    :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
 
   :profiles
-  {:uberjar {:omit-source true
+  {:uberjar       {:omit-source    true
 
-             :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
-             :aot :all
-             :uberjar-name "predictor.jar"
-             :source-paths ["env/prod/clj"]
-             :resource-paths ["env/prod/resources"]}
+                   :prep-tasks     ["compile" ["cljsbuild" "once" "min"]]
+                   :aot            :all
+                   :uberjar-name   "predictor.jar"
+                   :source-paths   ["env/prod/clj"]
+                   :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/test :profiles/test]
 
-   :project/dev  {:dependencies [[prone "1.1.1"]
-                                 [ring/ring-mock "0.3.0"]
-                                 [ring/ring-devel "1.5.0"]
-                                 [pjstadig/humane-test-output "0.8.0"]
-                                 [doo "0.1.6"]
-                                 [binaryage/devtools "0.7.0"]
-                                 [figwheel-sidecar "0.5.4-3"]
-                                 [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
+   :project/dev   {:dependencies   [[prone "1.1.1"]
+                                    [ring/ring-mock "0.3.0"]
+                                    [ring/ring-devel "1.5.0"]
+                                    [pjstadig/humane-test-output "0.8.0"]
+                                    [doo "0.1.6"]
+                                    [binaryage/devtools "0.7.0"]
+                                    [figwheel-sidecar "0.5.4-3"]
+                                    [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
 
-                                 [carry-debugger "0.5.0"]
-                                 [carry-logging "0.1.0"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.14.0"]
-                                 [lein-doo "0.1.6"]
-                                 [lein-figwheel "0.5.4-3"]
-                                 [org.clojure/clojurescript "1.9.36"]]
+                                    [carry-debugger "0.5.0"]
+                                    [carry-logging "0.1.0"]]
+                   :plugins        [[com.jakemccrary/lein-test-refresh "0.14.0"]
+                                    [lein-doo "0.1.6"]
+                                    [lein-figwheel "0.5.4-3"]
+                                    [org.clojure/clojurescript "1.9.36"]]
 
-                  :doo {:build "test"}
-                  :source-paths ["env/dev/clj" "test/clj"]
-                  :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user
-                                 :init (predictor.core/-main)}
-                  :injections [(require 'pjstadig.humane-test-output)
-                               (pjstadig.humane-test-output/activate!)]}
-   :project/test {:resource-paths ["env/dev/resources" "env/test/resources"]}
-   :profiles/dev {}
+                   :doo            {:build "test"}
+                   :source-paths   ["env/dev/clj" "test/clj"]
+                   :resource-paths ["env/dev/resources"]
+                   :repl-options   {:init-ns user
+                                    :init    (predictor.core/-main)}
+                   :injections     [(require 'pjstadig.humane-test-output)
+                                    (pjstadig.humane-test-output/activate!)]}
+   :project/test  {:resource-paths ["env/dev/resources" "env/test/resources"]}
+   :profiles/dev  {}
    :profiles/test {}})
