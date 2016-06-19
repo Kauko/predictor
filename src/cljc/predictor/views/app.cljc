8899067b817c6afdfb653840f62a611f556f2354
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2016-06-19 10:05:29 +0300

    Move boilerplate to core.cljs, figwheel works properly

--- src/cljc/predictor/views/app.cljc
+++ src/cljc/predictor/views/app.cljc
@@ -11,35 +11,5 @@
   [{:keys [counter] :as _view-model} dispatch]
   [:p
    (str "#" (:val (rum/react counter))) " "
-   [:button {:on-click #(dispatch :on-increment)} "+"] " "
+   [:button {:on-click #(dispatch :on-increment)} "Inc"] " "
    [:button {:on-click #(dispatch :on-decrement)} "-"] " "])
-
-
-(def spec {:initial-model model/-initial-model
-           :control       control/-control
-           :reconcile     reconciler/-reconcile})
-
-#?(:cljs
-   (defn main
-     []
-     (let [app (carry/app spec)
-           [app-view-model app-view] (carry-rum/connect app view-model/view-model view)]
-       (rum/mount app-view (.getElementById js/document "app"))
-       ((:dispatch-signal app) :on-start)
-       (assoc app :view-model app-view-model))))
-
-#?(:cljs
-   (defn mount! [mount-el]
-     (main)))
-
-;;;;;;;;;;;;;;;;;;;;;;;; Figwheel stuff
-#?(:cljs
-   (defn before-jsload
-     []
-     ;((:dispatch-signal app) :on-stop)
-     )
-   )
-
-#?(:cljs(defn on-jsload
-   []
-   #_(. js/console clear)))
--- src/cljs/predictor/core.cljs
+++ src/cljs/predictor/core.cljs
@@ -5,7 +5,14 @@
             [markdown.core :refer [md->html]]
             [predictor.ajax :refer [load-interceptors!]]
             [ajax.core :refer [GET POST]]
-            [predictor.views.app :as app])
+            [predictor.views.app :as app]
+            [rum.core :as rum]
+            [predictor.view-models.app :as view-model]
+            [predictor.carry-rum :as carry-rum]
+            [carry.core :as carry]
+            [predictor.reconcilers.app :as reconciler]
+            [predictor.controls.app :as control]
+            [predictor.models.app :as model])
   (:import goog.History))
 
 
@@ -14,29 +21,51 @@
 #_(secretary/set-config! :prefix "#")
 
 #_(secretary/defroute "/" []
-  (session/put! :page :home))
+                      (session/put! :page :home))
 
 #_(secretary/defroute "/about" []
-  (session/put! :page :about))
+                      (session/put! :page :about))
 
 ;; -------------------------
 ;; History
 ;; must be called after routes have been defined
 (defn hook-browser-navigation! []
   (doto (History.)
-        (events/listen
-          HistoryEventType/NAVIGATE
-          (fn [event]
-              (secretary/dispatch! (.-token event))))
-        (.setEnabled true)))
+    (events/listen
+      HistoryEventType/NAVIGATE
+      (fn [event]
+        (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
 
 ;; -------------------------
 ;; Initialize app
 
-(defn mount-components []
-  (app/mount! (js/document.getElementById "app")))
+(defonce app nil)
+
+(def spec {:initial-model model/-initial-model
+           :control       (control/-new-control)
+           :reconcile     reconciler/-reconcile})
+
+(defn mount! [component]
+  (rum/mount component (js/document.getElementById "app")))
+
+(defn main []
+  (let [app (carry/app spec)
+        [app-view-model app-view] (carry-rum/connect app view-model/view-model app/view)]
+    (mount! app-view)
+    ((:dispatch-signal app) :on-start)
+    (assoc app :view-model app-view-model)))
 
 (defn init! []
+  (js/console.log "Init!")
   (load-interceptors!)
   #_(hook-browser-navigation!)
-  (mount-components))
+  (set! app (main)))
+
+;;;;;;;;;;;;;;;;;;;;;;;; Figwheel stuff
+(defn before-jsload []
+  ((:dispatch-signal app) :on-stop))
+
+(defn on-jsload []
+  (mount! (app/view (view-model/view-model (:model app)) (:dispatch-signal app)))
+  #_(. js/console clear))
